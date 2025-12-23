9b4322c5e8d0b83556e53c586fa5aa5a99a3f1f1
parsavat <Shekarriz@hotmail.com>
2025-12-23 15:40:04 +0330

    Fix: Add CSP headers to allow Farcaster iframe embedding

--- next.config.js
+++ next.config.js
@@ -4,9 +4,26 @@ const nextConfig = {
   images: {
     domains: ['i.imgur.com', 'imagedelivery.net'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.farcaster.xyz https://*.warpcast.com https://*.farcaster.com https://farcaster.xyz https://warpcast.com;",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
+
+
+
+
 
 
 
